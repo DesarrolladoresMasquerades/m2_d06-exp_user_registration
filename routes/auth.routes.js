f7f30c1839a111970c461dfd528a676067a42f82
marcosantonastasi <marco.santonastasi@gmail.com>
2022-02-14 17:13:13 +0100

    done authentication

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,24 +18,64 @@ router
       res.render("signup", { errorMessage: "All filds are required" });
     }
 
-    User.find({ username }).then((user) => {
-      if (user) {
+    User.findOne({ username }).then((user) => {
+      if (user && user.username) {
         res.render("signup", { errorMessage: "User already taken!" });
+        throw new Error("Validation error");
       }
 
       const salt = bcrypt.genSaltSync(saltRounds);
       const hashedPwd = bcrypt.hashSync(password, salt);
 
-      User.create({ username, password: hashedPwd });
+      User.create({ username, password: hashedPwd }).then(() =>
+        res.redirect("/index")
+      );
     });
   });
 
-router.get("/login", (req, res) => {
-  res.render("login");
-});
+router
+  .route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+      res.render("login", { errorMessage: "All filds are required" });
+      throw new Error("Validation error");
+    }
+
+    User.findOne({ username })
+      .then((user) => {
+        if (!user) {
+          res.render("login", { errorMessage: "Incorrect credentials!" });
+          throw new Error("Validation error");
+        }
+
+        const isPwCorrect = bcrypt.compareSync(password, user.password);
+
+        if (isPwCorrect) {
+          req.session.currentUserId = user._id;
+          res.redirect("/profile");
+        } else {
+          res.render("login", { errorMessage: "Incorrect credentials!" });
+        }
+      })
+      .catch((error) => console.log(error));
+  });
 
 router.get("/profile", (req, res) => {
-  res.render("profile");
+  const id = req.session.currentUserId;
+  User.findById(id)
+    .then((user) => res.render("/profile", user))
+    .catch(err=>console.log(err));
 });
+
+router.get("/logout", ()=>{
+  req.session.destroy((err)=>res.redirect("/")
+  )
+})
 
 module.exports = router;
